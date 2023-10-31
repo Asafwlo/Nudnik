41c12daa5e6e5883935abcee547e6c9949cabd55
Asaf Wlo <wloasaf@gmail.com>
2023-10-31 23:19:29 +0200

    add reminder types

---
+++ components/types.tsx
@@ -0,0 +1,22 @@
+type Entity = {
+    id: string;
+}
+
+type Reminder = Entity & {
+    name: string;
+    typeId: string;
+    priority: 'regular' | 'important';
+    status: 'live' | 'done' | 'snooze';
+}
+
+type ReminderOnce = Entity & {
+    dateTime: Date;
+}
+
+type ReminderRepeat = Entity & {
+    startDate: Date;
+    endDate?: Date;
+    every?: number;
+    repeatType: 'day' | 'week' | 'weekDay' | 'month' | 'year' | 'start' | 'end';
+    history: Date[];
+}
